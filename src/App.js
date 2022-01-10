286c9caa4b88536021df4a10edfcf35b09218903
Pedro Padilha <pedropadilha@me.com>
2022-01-10 08:56:53 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Eu sou o perdo você não é o perdo
         </p>
         <a
           className="App-link"
